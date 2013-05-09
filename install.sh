d27f30fac6dc7322c7afff4c756842c3599ea131
Will Washburn <will.washburn@gmail.com>
2013-05-09 18:53:05 -0400

    add install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitrc ~/.gitrc
