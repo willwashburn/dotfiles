5ae9375812d22e212c27d832d37e3c1ece979f9d
Will Washburn <will.washburn@gmail.com>
2017-10-03 00:27:42 -0400

    Add hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,98 @@
+-----------------------------------------------
+-- Setup
+-----------------------------------------------
+hs.window.animationDuration = 0
+
+-----------------------------------------------
+-- cmd-left for left one half window
+-----------------------------------------------
+hs.hotkey.bind({"cmd"}, "Left", function()
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-----------------------------------------------
+-- cmd-ctrl-left for left third window
+-----------------------------------------------
+hs.hotkey.bind({"cmd","ctrl"}, "Left", function()
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-----------------------------------------------
+-- cmd-ctrl-left for left 2/3 window
+-----------------------------------------------
+hs.hotkey.bind({"cmd","option"}, "Left", function()
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = (max.w / 3) *2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-----------------------------------------------
+-- cmd-right for right one half window
+-----------------------------------------------
+hs.hotkey.bind({"cmd"}, "Right", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-----------------------------------------------
+-- cmd-ctrl-right for right third window
+-----------------------------------------------
+hs.hotkey.bind({"cmd","ctrl"}, "Right", function()
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 3) + (max.w / 3)
+  f.y = max.y
+  f.w = max.w / 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-----------------------------------------------
+-- Reload config on write
+-----------------------------------------------
+function reload_config(files)
+  hs.reload()
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.alert.show("Config loaded")
+
--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitrc ~/.gitrc
 ln -s ~/.dotfiles/iterm/profiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/profiles.json
+ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
