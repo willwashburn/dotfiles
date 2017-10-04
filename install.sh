03a26ee478ccbb91767f090590ed7871759ecbd6
Will Washburn <will.washburn@gmail.com>
2017-10-04 11:25:18 -0400

    Setup gpg more appropriately

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitrc ~/.gitrc
 ln -s ~/.dotfiles/iterm/profiles.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/profiles.json
 ln -s ~/.dotfiles/hammerspoon ~/.hammerspoon
+ln -s ~/.dotfiles/gpg/gpg.conf ~/.gnupg/gpg.conf
+ln -s ~/.dotfiles/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
